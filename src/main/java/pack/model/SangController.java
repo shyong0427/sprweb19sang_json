a944bb27b2b995f17d1333008b0f467179cbb287
shyong0427 <shyong0427@daum.net>
2019-10-11 11:39:08 +0900

    2019-10-11 Spring_Web_Json Update Ver.1

---
+++ src/main/java/pack/model/SangController.java
@@ -0,0 +1,41 @@
+package pack.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SangController {
+	@Autowired
+	private DataDao dataDao;
+	
+	@RequestMapping("sangpum")
+	@ResponseBody
+	public Map<String, Object> sangpums() {
+		List<Map<String, String>> dataList = new ArrayList<Map<String, String>>();
+		Map<String, String> data = null;
+		
+		List<SangpumDto> sangdatas = dataDao.sangpumList();
+		
+		for(SangpumDto s : sangdatas) {
+			data = new HashMap<String, String>();
+			data.put("code", s.getCode());
+			data.put("sang", s.getSang());
+			data.put("su", s.getSu());
+			data.put("dan", s.getDan());
+			
+			dataList.add(data);
+		}
+		
+		Map<String, Object> sangpumDatas = new HashMap<String, Object>();
+		sangpumDatas.put("datas", dataList);
+		
+		return sangpumDatas;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>초기 화면</title>
+</head>
+<body>
+	<a href="sangpum">상품목록(json으로 출력)</a><br>
+	<a href="sanlist.jsp">상품목록(ajax로 처리)</a><br>
+</body>
+</html>
